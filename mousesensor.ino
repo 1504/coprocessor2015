0c8d0e2732d49a8c20be5f41a3dca624fd8a03b5
Ajusa Jain <arhamjain@gmail.com>
2015-02-05 20:54:34 -0500

    Fixed the Magnetometer not updating

--- mousesensor.ino
+++ mousesensor.ino
@@ -2,13 +2,14 @@
 #include <Wire.h>
 #include <adns2620.h>
 ADNS2620 mouse1(3,4);
-ADNS2620 mouse2(5,6);
+ADNS2620 mouse2(8,5);
 
 char mouse1Val;
 char mouse2Val;
 char surface;
 char magX, magY, magZ;
 HMC5883L compass;
+int error = 0;
 void setup() {
     mouse1.begin();
     delay(100);
@@ -19,14 +20,15 @@ void setup() {
     mouse2.sync();
     mouse2.write(CONFIGURATION_REG, 0x01);
     Serial.begin(9600);
-    compass = HMC5883L();
     Wire.begin();
+    compass = HMC5883L();
+    compass.SetMeasurementMode(Measurement_Continuous);
 
 }
 
 void loop() {
-  if(Serial.available() > -1){
-    /*First mouse thingy
+  if(Serial.available() > 2){
+    //First mouse thingy
     mouse1Val = mouse1.read(DELTA_X_REG);
     Serial.write(mouse1Val);
     mouse1Val = mouse1.read(DELTA_Y_REG);
@@ -35,21 +37,18 @@ void loop() {
     Serial.write(surface);
     //Second mouse thingy
     mouse2Val = mouse2.read(DELTA_X_REG);
-    Serial.write(mouse2Val);
+    Serial.print(mouse2Val, HEX);
     mouse2Val = mouse2.read(DELTA_Y_REG);
-    Serial.write(mouse2Val);
+    Serial.print(mouse2Val, HEX);
     surface = mouse2.read(SQUAL_REG);
-    Serial.write(surface);
-    */
-    //Magometer, reads and write
+    Serial.println(surface);
+    //Magnetometer, reads and write
     MagnetometerRaw raw = compass.ReadRawAxis();
-    Serial.print(raw.XAxis);
-    Serial.print('\t');
-    Serial.print(raw.YAxis);
-    Serial.print('\t');
-    Serial.println(raw.ZAxis);
-}
-delay(30); 
+    Serial.write(raw.XAxis);
+    Serial.write(raw.YAxis);
+    Serial.write(raw.ZAxis);
+  }
+delay(8); 
 }
 /*
 mousel
@@ -64,5 +63,4 @@ mag
 x
 y
 z
-
 */
